21cf17055f20808ad5b6c66217087e0916171031
Maciej Zieminski <ziemac1@gmail.com>
2023-04-30 14:24:32 +0200

    Finished stopper app

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -5,7 +5,3 @@ const Button = props => {
 };
 
 export default Button;
-
-//https://github.com/rmartyniuk/stoper/blob/master/src/App.js
-// https://github.com/DagmaraJaneczek/project-pizzeria
-// https://github.com/Arangus10?tab=repositories
