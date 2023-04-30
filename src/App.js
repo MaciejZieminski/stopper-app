7ce12b335117136b1b5b773f56e9671048dcdcc5
Maciej Zieminski <ziemac1@gmail.com>
2023-04-30 14:10:49 +0200

    Finished stopper app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import Stopper from './components/Stopper/Stopper.js';
+import { useState, useEffect } from 'react';
+import Container from './components/Container/Container';
+import FormattedTime from './components/FormattedTime/FormattedTime';
+import Button from './components/Button/Button';
 
 const App = () => {
+  const [time, setTime] = useState(0);
+  const [timer, setTimer] = useState(null);
+
+  const start = () => {
+    setTimer(setInterval(() => {
+      setTime(prevValue => prevValue +1);
+    }, 1))
+  };
+
+  const stop = () => {
+    clearInterval(timer);
+    setTimer();
+  };
+
+  const reset = () => {
+    setTime(0);
+    setTimer(null);
+  }
+
+  useEffect(() => {
+    return () => {
+      if(timer) clearInterval(timer);
+    };
+  }, [timer]);
+
   return (
-    <div>
-      <Stopper />
-    </div>
-  )
+    <Container>
+      <FormattedTime time={time}/>
+      <Button value={start}>start</Button>
+      <Button value={stop}>stop</Button>
+      <Button value={reset}>reset</Button>
+    </Container>
+  );
 }
 
 export default App;
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,11 @@
-import styles from './Button.modules.scss';
+import styles from './Button.module.scss';
 
-const Button = (props) => {
-    return(
-       <button className={styles.button}>{props.children}</button> 
-    )
-}
+const Button = props => {
+    return <button className={styles.button} onClick={props.value}>{props.children}</button>
+};
+
+export default Button;
+
+//https://github.com/rmartyniuk/stoper/blob/master/src/App.js
+// https://github.com/DagmaraJaneczek/project-pizzeria
+// https://github.com/Arangus10?tab=repositories
